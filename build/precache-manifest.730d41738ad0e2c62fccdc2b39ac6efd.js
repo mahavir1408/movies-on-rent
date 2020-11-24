b9b74602d910be7f7f02983037aab3ab8f97592a
Mahavir Munot <mahavir1408@gmail.com>
2020-11-24 20:30:36 +0530

    code cleanup

---
+++ build/precache-manifest.730d41738ad0e2c62fccdc2b39ac6efd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6a0c527dbdc1f072efd22f4721e0e4",
+    "url": "/movies-on-rent/index.html"
+  },
+  {
+    "revision": "2cda7ee82fc220f2f5eb",
+    "url": "/movies-on-rent/static/css/2.96d3a308.chunk.css"
+  },
+  {
+    "revision": "b7c74e445647ee96d82d",
+    "url": "/movies-on-rent/static/css/main.035ff2b0.chunk.css"
+  },
+  {
+    "revision": "2cda7ee82fc220f2f5eb",
+    "url": "/movies-on-rent/static/js/2.73c8e624.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/movies-on-rent/static/js/2.73c8e624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c74e445647ee96d82d",
+    "url": "/movies-on-rent/static/js/main.cc1a2c06.chunk.js"
+  },
+  {
+    "revision": "9be3ac55791973107581",
+    "url": "/movies-on-rent/static/js/runtime-main.e21cec46.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies-on-rent/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies-on-rent/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies-on-rent/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies-on-rent/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies-on-rent/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
